b2bc59634315cd943c345f0cbfe6a237d040e431
Dhiraj Arun <dhrjarun@gmail.com>
2021-04-16 18:28:29 +0530

    added the pagination feature

--- src/components/movie.jsx
+++ src/components/movie.jsx
@@ -1,27 +1,55 @@
-import React, { Component } from 'react';
-import Table from './table';
-import Pagination from './pagination';
-import {getMovies, getmovie, saveMovie, deleteMovie} from '../services/fakeMovieService';
+import React, { Component } from "react";
+import Table from "./table";
+import Pagination from "./pagination";
+import { getMovies } from "../services/fakeMovieService";
+import _ from "underscore";
 
 class Movie extends Component {
-    state = {
-        movies: getMovies()
-    }
+  state = {
+    movies: getMovies(),
+    currentPage: 1,
+  };
 
-    handleDelete = (movie) => {
-        const sMovies = [...this.state.movies];
-        const movies = sMovies.filter((value) => value._id != movie._id);
-        this.setState({ movies });
-    }
+  getViewedMovies() {
+    const { movies, currentPage } = this.state;
+    return movies.slice((currentPage - 1) * 4, currentPage * 4);
+  }
 
-    render() { 
-        return (  
-            <div>
-                <Table movies={this.state.movies} onDelete={this.handleDelete} />
-                <Pagination />
-            </div>
-        );
+  getPages() {
+    const moviLength = this.state.movies.length;
+    const totalPage =
+      moviLength / 4 === Math.floor(moviLength / 4)
+        ? moviLength / 4
+        : Math.floor(moviLength / 4) + 1;
+    const pages = _.range(1, totalPage + 1);
+    return pages;
+  }
+
+  handleDelete = (movie) => {
+    const sMovies = [...this.state.movies];
+    const currentPage = this.state.currentPage;
+    const movies = sMovies.filter((value) => value._id != movie._id);
+    this.setState({ movies });
+    if (movies.length <= currentPage * 4 - 4 && currentPage > 1) {
+      this.setState({ currentPage: currentPage - 1 });
     }
+  };
+
+  handlePageChange = (pageNo) => {
+    this.setState({ currentPage: pageNo });
+  };
+
+  render() {
+    return (
+      <div>
+        <Table movies={this.getViewedMovies()} onDelete={this.handleDelete} />
+        <Pagination
+          pages={this.getPages()}
+          onPageChange={this.handlePageChange}
+        />
+      </div>
+    );
+  }
 }
- 
+
 export default Movie;
--- src/components/table.jsx
+++ src/components/table.jsx
@@ -1,43 +1,57 @@
-import React, { Component } from 'react';
-import {getMovies, getmovie, saveMovie, deleteMovie} from '../services/fakeMovieService';
+import React, { Component } from "react";
+import {
+  getMovies,
+  getmovie,
+  saveMovie,
+  deleteMovie,
+} from "../services/fakeMovieService";
 
 class Table extends Component {
-    render() {
-        const {movies, onDelete} = this.props;
-        const moviLength = movies.length;
-        return (
-            <div>
-                {moviLength === 0 ? <p>There are no movies</p> : <p>Showing {moviLength} movies from database</p>}
-                <table>
-                    <thead>
-                        {
-                            moviLength > 0 && 
-                            <tr>
-                                <th>Title</th>
-                                <th>Genre</th>
-                                <th>Stock</th>
-                                <th>Rate</th>
-                                <th></th>
-                            </tr>
-                        }
-                    </thead>
-                    <tbody>
-                        {movies.map((movie)=> {
-                            return (
-                                <tr key={movie._id}>
-                                    <td>{movie.title}</td>
-                                    <td>{movie.genre.name}</td>
-                                    <td>{movie.numberInStock}</td>
-                                    <td>{movie.dailyRentalRate}</td>
-                                    <td onClick={()=>{onDelete(movie)}}><button>Delete</button></td>
-                                </tr>
-                            );
-                        })}
-                    </tbody>
-                </table>
-            </div>
-        );
-    }
+  render() {
+    const { movies, onDelete } = this.props;
+    const moviLength = movies.length;
+    return (
+      <div>
+        {moviLength === 0 ? (
+          <p>There are no movies</p>
+        ) : (
+          <p>Showing {moviLength} movies from database</p>
+        )}
+        <table>
+          <thead>
+            {moviLength > 0 && (
+              <tr>
+                <th>Title</th>
+                <th>Genre</th>
+                <th>Stock</th>
+                <th>Rate</th>
+                <th></th>
+              </tr>
+            )}
+          </thead>
+          <tbody>
+            {movies.map((movie) => {
+              return (
+                <tr key={movie._id}>
+                  <td>{movie.title}</td>
+                  <td>{movie.genre.name}</td>
+                  <td>{movie.numberInStock}</td>
+                  <td>{movie.dailyRentalRate}</td>
+                  <td
+                    onClick={() => {
+                      onDelete(movie);
+                    }}
+                  >
+                    <button>Delete</button>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
 }
- 
+
 export default Table;
